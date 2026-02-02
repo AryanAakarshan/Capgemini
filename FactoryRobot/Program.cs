a67d5564934568cd2a33e9c4338e37c5dea3f5fa
Aryan Aakarshan <122559823+AryanAakarshan@users.noreply.github.com>
2026-02-02 14:38:18 +0530

    Add files via upload

---
+++ FactoryRobot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;;
+
+
+class Program
+{
+    public double CalculateHazardRisk(double armPrecision, int workerDensity, string machineryState)
+    {
+        
+    }
+    static void Main()
+    {
+        
+    }
+}
